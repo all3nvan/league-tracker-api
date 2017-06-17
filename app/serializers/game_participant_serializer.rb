1bfb82facdd5171a610910ee26d71b02a9811a56
Allen Van <all3nvan@gmail.com>
2017-06-17 00:45:43 -0700

    Serialize game participant id in output so that client can use the id when updating game participant.

--- app/serializers/game_participant_serializer.rb
+++ app/serializers/game_participant_serializer.rb
@@ -1,5 +1,5 @@
 class GameParticipantSerializer < ActiveModel::Serializer
   attribute :summoner_id, key: :summonerId
   attribute :champion_id, key: :championId
-  attributes :team, :kills, :deaths, :assists, :win
+  attributes :id, :team, :kills, :deaths, :assists, :win
 end
