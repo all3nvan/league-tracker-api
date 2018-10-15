76c0fc16afdcbb31f73569461984e2906a8a38b2
Allen Van <all3nvan@gmail.com>
2018-10-14 18:32:54 -0700

    Fix integration test for previous commit.

--- spec/requests/games_api_spec.rb
+++ spec/requests/games_api_spec.rb
@@ -24,15 +24,7 @@ RSpec.describe 'Games API', type: :request do
       # TODO: Prob should have a test on the Serializer for these instead.
       expect(game_json['game']['gameId']).to eq(2513935315)
       expect(game_json['game']['createTime']).to eq('2017-06-02T05:33:24.000Z')
-      expect(game_json['game']['gameParticipants'].length).to eq(10)
-      game_json['game']['gameParticipants'].each do |game_participant|
-        expect(game_participant['championId']).to_not be_nil
-        expect(game_participant['team']).to_not be_nil
-        expect(game_participant['kills']).to_not be_nil
-        expect(game_participant['deaths']).to_not be_nil
-        expect(game_participant['assists']).to_not be_nil
-        expect(game_participant['win']).to_not be_nil
-      end
+      expect(game_json['game']['gameParticipantIds'].length).to eq(10)
     end
   end
 end
